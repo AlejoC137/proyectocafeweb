b4f891d70bf24266db4bac892f7d8a7dd355480a
AlejoC137 <dapatinoa@unal.edu.co>
2024-12-23 13:24:47 -0500

    26.6

--- src/components/ui/cardGridInventarioMenu.jsx
+++ src/components/ui/cardGridInventarioMenu.jsx
@@ -1,19 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { CardInstanceInventario } from "@/components/ui/cardInstanceInventario";
-import { copiarAlPortapapeles } from "../../redux/actions";
-import { ItemsAlmacen, ProduccionInterna } from "../../redux/actions-types";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import { CardInstanceInventarioMenu } from "@/components/ui/cardInstanceInventarioMenu";
 
 export function CardGridInventarioMenu({ products, showEdit }) {
+  const groupedProducts = products.reduce((acc, product) => {
+    const group = product.GRUPO || "Sin Grupo";
+    if (!acc[group]) {
+      acc[group] = [];
+    }
+    acc[group].push(product);
+    return acc;
+  }, {});
+
   return (
     <div className="flex flex-col gap-2 ml-4 mr-4">
-      {products.map((product) => (
-        <CardInstanceInventarioMenu
-          key={product._id}
-          product={product}
-          showEdit={showEdit}
-        />
+      {Object.keys(groupedProducts).map((group) => (
+        <div key={group}>
+          <h2 className="text-xl font-bold">{group}</h2>
+          {groupedProducts[group].map((product) => (
+            <CardInstanceInventarioMenu
+              key={product._id}
+              product={product}
+              showEdit={showEdit}
+            />
+          ))}
+        </div>
       ))}
     </div>
   );
